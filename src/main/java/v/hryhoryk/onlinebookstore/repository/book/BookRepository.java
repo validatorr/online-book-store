dc2d118a2695b688972d4b712ff784014e0391be
Volodymyr Hryhoryk <volodymyrhryhoryk@gmail.com>
2023-10-24 09:38:21 +0300

    @Repository in repository layer added

--- src/main/java/v/hryhoryk/onlinebookstore/repository/book/BookRepository.java
+++ src/main/java/v/hryhoryk/onlinebookstore/repository/book/BookRepository.java
@@ -2,7 +2,9 @@ package v.hryhoryk.onlinebookstore.repository.book;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
 import v.hryhoryk.onlinebookstore.model.Book;
 
+@Repository
 public interface BookRepository extends JpaRepository<Book, Long>, JpaSpecificationExecutor<Book> {
 }
