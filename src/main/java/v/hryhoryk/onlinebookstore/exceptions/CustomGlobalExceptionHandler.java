64da04bcc0bb2b7bf1ad3fb285e9b64af65c60ba
Volodymyr Hryhoryk <volodymyrhryhoryk@gmail.com>
2023-10-26 06:33:51 +0300

    mvn fixed

--- src/main/java/v/hryhoryk/onlinebookstore/exceptions/CustomGlobalExceptionHandler.java
+++ src/main/java/v/hryhoryk/onlinebookstore/exceptions/CustomGlobalExceptionHandler.java
@@ -3,7 +3,6 @@ package v.hryhoryk.onlinebookstore.exceptions;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.http.HttpHeaders;
