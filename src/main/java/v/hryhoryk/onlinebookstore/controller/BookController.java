5b48c08039e372b85861f05a22ed16120793973f
Volodymyr Hryhoryk <volodymyrhryhoryk@gmail.com>
2023-10-26 07:07:56 +0300

    BookController updated

--- src/main/java/v/hryhoryk/onlinebookstore/controller/BookController.java
+++ src/main/java/v/hryhoryk/onlinebookstore/controller/BookController.java
@@ -5,10 +5,13 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Positive;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -27,6 +30,7 @@ import v.hryhoryk.onlinebookstore.service.BookService;
         description = "Endpoints for managing book store API")
 @RequiredArgsConstructor
 @RestController
+@Validated
 @RequestMapping(value = "/books")
 public class BookController {
     private final BookService bookService;
@@ -41,7 +45,8 @@ public class BookController {
     })
     @ResponseStatus(HttpStatus.OK)
     @GetMapping
-    public List<BookDto> getAll(Pageable pageable) {
+    public List<BookDto> getAll(
+            @PageableDefault Pageable pageable) {
         return bookService.getAll(pageable);
     }
 
@@ -55,7 +60,8 @@ public class BookController {
     })
     @ResponseStatus(HttpStatus.OK)
     @GetMapping("/{id}")
-    public BookDto getBookById(@PathVariable Long id) {
+    public BookDto getBookById(
+            @PathVariable @Positive Long id) {
         return bookService.getById(id);
     }
 
@@ -69,7 +75,8 @@ public class BookController {
     })
     @ResponseStatus(HttpStatus.OK)
     @GetMapping("/search")
-    public List<BookDto> search(BookSearchParameters searchParameters, Pageable pageable) {
+    public List<BookDto> search(
+            BookSearchParameters searchParameters, Pageable pageable) {
         return bookService.search(searchParameters, pageable);
     }
 
@@ -83,7 +90,8 @@ public class BookController {
     })
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public BookDto createBook(@RequestBody @Valid CreateBookRequestDto requestDto) {
+    public BookDto createBook(
+            @RequestBody @Valid CreateBookRequestDto requestDto) {
         return bookService.createBook(requestDto);
     }
 
@@ -97,7 +105,8 @@ public class BookController {
     })
     @ResponseStatus(HttpStatus.OK)
     @PutMapping("/{id}")
-    public BookDto updateById(@PathVariable Long id, @RequestBody CreateBookRequestDto book) {
+    public BookDto updateById(
+            @PathVariable @Positive Long id, @RequestBody @Valid CreateBookRequestDto book) {
         return bookService.updateById(id, book);
     }
 
@@ -111,7 +120,8 @@ public class BookController {
     })
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @DeleteMapping("/{id}")
-    public void deleteById(@PathVariable Long id) {
+    public void deleteById(
+            @PathVariable @Positive Long id) {
         bookService.deleteById(id);
     }
 }
